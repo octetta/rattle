22374ae700368764946a8fa284e91210f95d2f76
octetta <joseph.stewart@gmail.com>
2024-01-29 12:37:44 -0800

    Playing with timers...

--- src/timer02.go
+++ src/timer02.go
@@ -10,22 +10,30 @@ func main() {
     ticker := time.NewTicker(250 * time.Millisecond)
     done := make(chan bool)
 
+    g0 := 100
+
     go func() {
         l := time.Now()
         for {
             select {
-            case <-done:
+            case <- done:
                 return
-            case t := <-ticker.C:
+            case t := <- ticker.C:
                 //fmt.Println("Tick at", t)
                 //fmt.Println("Last at", l)
-                fmt.Println("diff", t.Sub(l).Milliseconds())
+                fmt.Println("[", g0, "]", "diff", t.Sub(l).Milliseconds())
                 l = t
             }
         }
     }()
 
-    time.Sleep(1000 * time.Millisecond)
+    time.Sleep(500 * time.Millisecond)
+    g0 = 200
+    time.Sleep(500 * time.Millisecond)
+    g0 = 355
+    time.Sleep(500 * time.Millisecond)
+    g0 = 113
+    time.Sleep(500 * time.Millisecond)
     ticker.Stop()
     done <- true
     fmt.Println("Ticker stopped")
