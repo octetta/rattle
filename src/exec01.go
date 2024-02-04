5133beafc02a0107cfd5d7a7aeb9f7fb7779f0c7
octetta <joseph.stewart@gmail.com>
2024-02-03 21:38:02 -0800

    Clean up.

--- src/exec01.go
+++ src/exec01.go
@@ -45,8 +45,6 @@ func framer(n int) {
 }
 
 var sample []int16
-var interval int64
-var latency int64
 
 func samples() []int16 {
     sample = make([]int16, frames())
@@ -161,8 +159,14 @@ func dump() {
   }
 }
 
-func runner(done chan bool, metro chan int) {
-  //l := time.Now()
+var interval int64
+var latency int64
+
+var done chan bool
+var metro chan int
+
+func runner() {
+  l := time.Now()
   interval := 250
   ticker := time.NewTicker(time.Duration(interval) * time.Millisecond)
   for {
@@ -171,10 +175,9 @@ func runner(done chan bool, metro chan int) {
         return
       case interval = <- metro:
         ticker.Reset(time.Duration(interval) * time.Millisecond)
-      case _ = <- ticker.C:
-      //case t := <- ticker.C:
-        //diff = t.Sub(l).Milliseconds()
-        //latency = diff
+      case t := <- ticker.C:
+        diff := t.Sub(l).Milliseconds()
+        latency = diff
         c := clk()
         for i:=0; i<len(ptr); i++ {
           if run[i] == 0 {
@@ -192,12 +195,12 @@ func runner(done chan bool, metro chan int) {
             ptr[i] = p+1
           }
         }
-        //l = t
+        l = t
     }
   }
 }
 
-func toker(tok string, now int, done chan bool, metro chan int) int {
+func toker(tok string, now int) int {
   re1 := regexp.MustCompile(`:[0-9]/*`)
   switch {
     case tok == "@":
@@ -228,7 +231,7 @@ func toker(tok string, now int, done chan bool, metro chan int) int {
         process("v20w1f110", now)
         //process("v30w7p5", now)
         process("v30w1f220", now)
-        go runner(done, metro)
+        go runner()
       }
     case tok == "@1":
       done <- true
@@ -249,7 +252,6 @@ func toker(tok string, now int, done chan bool, metro chan int) int {
       if len(tok) > 1 {
         switch {
           case tok[:2] == ":q":
-            //goto exit
             return -1
           case tok[:2] == ":b":
             fmt.Println(int(C.rat_block_size()))
@@ -261,7 +263,6 @@ func toker(tok string, now int, done chan bool, metro chan int) int {
             if len(tok) > 2 {
               ms, err := strconv.ParseInt(tok[3:], 10, 64)
               if err == nil {
-                //ticker.Reset(time.Duration(ms) * time.Millisecond)
                 interval = ms
                 metro <- int(interval)
               }
@@ -403,7 +404,6 @@ func main() {
 
   C.rat_start()
 
-  //
   if usefile != "" {
     file, err := os.Open(usefile)
     if err != nil {
@@ -416,194 +416,49 @@ func main() {
       file.Close()
     }
   }
-  //
 
-    l,_ := readline.NewEx(&readline.Config{
-      Prompt: "# ",
-      HistoryFile: "/tmp/readline.tmp",
-      InterruptPrompt: "^C",
-    })
-    defer l.Close()
-    l.CaptureExitSignal()
+  l,_ := readline.NewEx(&readline.Config{
+    Prompt: "# ",
+    HistoryFile: "/tmp/readline.tmp",
+    InterruptPrompt: "^C",
+  })
+  defer l.Close()
+  l.CaptureExitSignal()
 
-    time.Sleep(time.Duration(interval) * time.Millisecond)
+  time.Sleep(time.Duration(interval) * time.Millisecond)
 
-    //var sample = make([]int16, 256)
+  done = make(chan bool)
+  metro = make(chan int)
 
-    done := make(chan bool)
-    metro := make(chan int)
-    //var diff int64
-
-    //re1 := regexp.MustCompile(`:[0-9]/*`)
-    for {
-      line,err := l.Readline()
-      if err == readline.ErrInterrupt {
-        if len(line) == 0 {
-          break
-        } else {
-          continue
-        }
-      } else if err == io.EOF {
-        break
-      }
-      line = strings.TrimSpace(line)
+  //re1 := regexp.MustCompile(`:[0-9]/*`)
+  for {
+    line,err := l.Readline()
+    if err == readline.ErrInterrupt {
       if len(line) == 0 {
+        break
+      } else {
         continue
       }
-      a := strings.Split(line, ";")
-      now := clk()
-      for _, ptok := range a {
-        tok := strings.TrimSpace(ptok)
-        if len(tok) == 0 {
-          continue
-        }
-
-        /*
-        switch {
-          case tok == "@":
-            for i:=0; i<len(pat); i++ {
-              c := 0
-              for j:=0; j<len(pat[i]); j++ {
-                if len(pat[i][j]) == 0 {
-                  break
-                }
-                c++
-              }
-              if c == 0 {
-                continue
-              }
-              fmt.Printf("# %d len:%d ptr:%d\n", i, c, ptr[i])
-              for j:=0; j<len(pat[i]); j++ {
-                if len(pat[i][j]) == 0 {
-                  break
-                }
-                fmt.Printf(":%d/%d=%s\n",i,j,pat[i][j])
-              }
-            }
-          case tok == "@0":
-            if len(tok) > 1 {
-              process("S20", now)
-              process("S30", now)
-              //process("v20w7p45", now)
-              process("v20w1f110", now)
-              //process("v30w7p5", now)
-              process("v30w1f220", now)
-              go runner(done, metro)
-            }
-          case tok == "@1":
-            done <- true
-          case tok == "@2":
-            pat[0] = append(pat[0], "v20l5")
-            pat[0] = append(pat[0], "v20l0")
-          case tok == "@3":
-            pat[1] = append(pat[1], "v30l1")
-            pat[1] = append(pat[1], "v30l0")
-            pat[1] = append(pat[1], "v40l1")
-            pat[1] = append(pat[1], "v40l0")
-          case tok == "@4":
-            pat[2][0] = "#"
-          case tok == "::":
-            dump()
-          case tok[:1] == ":":
-            // : = system settings
-            if len(tok) > 1 {
-              switch {
-                case tok[:2] == ":q":
-                  goto exit
-                case tok[:2] == ":b":
-                  fmt.Println(int(C.rat_block_size()))
-                case tok[:2] == ":r":
-                  fmt.Println(int(C.rat_sample_rate()))
-                case tok[:2] == ":o":
-                  fmt.Println(int(C.rat_oscs()))
-                case tok[:2] == ":m":
-                  if len(tok) > 2 {
-                    ms, err := strconv.ParseInt(tok[3:], 10, 64)
-                    if err == nil {
-                      //ticker.Reset(time.Duration(ms) * time.Millisecond)
-                      interval = ms
-                      metro <- int(interval)
-                    }
-                  } else {
-                    fmt.Println(interval)
-                  }
-                case matcher(":[0-9]", tok):
-                  n := int(tok[1]-48)
-                  _dump(n)
-                case matcher(":[0-9]/[prs]", tok):
-                  n := int(tok[1]-48)
-                  a := tok[3:]
-                  switch {
-                    case a == "p":
-                      //fmt.Println("pause")
-                      run[n] = 0
-                    case a == "r":
-                      //fmt.Println("resume")
-                      run[n] = 1
-                    case a == "s":
-                      //fmt.Println("stop")
-                      run[n] = 0
-                      ptr[n] = 0
-                  }
-                case re1.MatchString(tok):
-                  n := int(tok[1]-48)
-                  arg := tok[3:]
-                  b := strings.Split(arg, "=")
-                  m, _ := strconv.ParseInt(b[0], 10, 32)
-                  if m >= 0 && m < int64(PLEN) {
-                    pat[n][m] = b[1]
-                  }
-                case matcher(":[a-z]", tok):
-                  fmt.Println("match :[a-z]")
-                case matcher(":[a-z]=*", tok):
-                  fmt.Println("match :[a-z]=*")
-              }
-            }
-          case tok == "?p":
-            graph(samples())
-          case tok == "?i":
-            fmt.Println(frames())
-          case tok == "?n":
-            sample = samples()
-            fmt.Println(sample)
-          case tok == "?c":
-            fmt.Println(clk())
-          case tok == "?l":
-            fmt.Println(latency)
-          case tok[:1] == "~":
-            // ~ = pause
-            if len(tok) > 1 {
-              ms, _ := strconv.ParseInt(tok[1:], 10, 32)
-              time.Sleep(time.Duration(ms) * time.Millisecond)
-            } else {
-              time.Sleep(time.Duration(interval) * time.Millisecond)
-            }
-          case tok[:1] == "<":
-            // < = capture frames
-            if len(tok) > 1 {
-              ms, _ := strconv.ParseInt(tok[1:], 10, 32)
-              n := (ms * 44100) / 1000
-              framer(int(n))
-            } else {
-              framer(44100 * 2)
-            }
-          case tok[:1] == "/":
-            fmt.Println("/ = jump")
-          case tok[:1] == "$":
-            fmt.Println("$ = var")
-          case matcher("[a-zA-Z]*", tok):
-            //amy(tok)
-            process(tok, clk())
-          default:
-            fmt.Println("???", tok)
-        }
-        */
-        if toker(tok, now, done, metro) < 0 {
-          goto exit
-        }
+    } else if err == io.EOF {
+      break
+    }
+    line = strings.TrimSpace(line)
+    if len(line) == 0 {
+      continue
+    }
+    a := strings.Split(line, ";")
+    now := clk()
+    for _, ptok := range a {
+      tok := strings.TrimSpace(ptok)
+      if len(tok) == 0 {
+        continue
+      }
+      if toker(tok, now) < 0 {
+        goto exit
       }
     }
-    exit:
-    C.rat_stop()
-    fmt.Println("done")
+  }
+  exit:
+  C.rat_stop()
+  fmt.Println("done")
 }
